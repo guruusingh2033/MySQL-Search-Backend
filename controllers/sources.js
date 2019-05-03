7f290062ea9653689d2e736542f129faece9b948
guruusingh2033 <guruu.singh2033@gmail.com>
2019-05-03 19:51:06 +0530

    implemented data by tme functionality

--- controllers/sources.js
+++ controllers/sources.js
@@ -1,9 +1,9 @@
 module.exports = {
-    getSourcesByDevId
+    getSourcesByDevId,
+    getDataByTime
 }
 
 function getSourcesByDevId(body, connection) {
-    console.log(body.devID)
     return new Promise(function (resolve, reject) {
         var fetch = "SELECT * FROM messages WHERE devID = '" + body.devID + "'"
         connection.query(fetch, function (error, results) {
@@ -16,5 +16,23 @@ function getSourcesByDevId(body, connection) {
             }
         });
     })
+}
+
+//getting data by time
+function getDataByTime(body, connection) {
+    return new Promise(function (resolve, reject) {
+        var fetch = "SELECT * FROM messages WHERE datetime between '" + body.startDate + "' and '" + body.endDate + "' AND devID='" + body.devIdForSearch + "' AND source='" + body.sourceValue + "' AND message LIKE '%" + body.searchValue + "%'";
+        connection.query(fetch, function (error, results) {
+            var sourceData = results
+            if (results.length != 0) {
+                resolve(sourceData);
+            }
+            else {
+                reject({ "error": "No source data available" });
+            }
+        });
+    })
+
+
 
 }
--- routes/source.js
+++ routes/source.js
@@ -3,8 +3,20 @@ const source_controller = require('.././controllers/sources.js');
 var connection = require('../db.js');
 
 router.post('/getsourcesbydevid', function (req, res) {
-    console.log(req.body)
     source_controller.getSourcesByDevId(req.body, connection).then(function (response) {
+        res.status(200).send(response)
+    },
+        function (err) {
+            res.status(400).send(err)
+        })
+        .catch(function (exception) {
+            res.status(400).send(exception)
+        })
+})
+
+//getting data by time
+router.post('/getdatabytime', function (req, res) {
+    source_controller.getDataByTime(req.body, connection).then(function (response) {
         res.status(200).send(response)
     },
         function (err) {
